9c82af86c2adfa899d72c3ac55a8919eb3738733
Grant Parr <66440717+grantparr@users.noreply.github.com>
2021-03-28 11:37:17 -0500

    Add files via upload

---
+++ Parr.Grant.DSC530.Wk2.Asnmt1.py
@@ -0,0 +1,28 @@
+# Display the text “Hello World!”
+text = "Hello World!";
+print(text)
+# Add two numbers together
+2 + 4
+print(2 + 4)
+# Subtract a number from another number
+9 - 7
+print(9 - 7)
+# Multiply two numbers
+60 * 24
+print(60 * 24)
+# Divide between two numbers
+525600 / 365
+print(525600 / 365)
+# Concatenate two strings together (any words)
+player = "Roger Federer"
+adjective = "greatest of all time!"
+sentence = player + " " + "is the" + " " + adjective
+print(sentence)
+# Create a list of 4 items (can be strings, numbers, both)
+tennis_players = ["Roger Federer", "Tennis Player", 39, 20]
+# Append an item to your list (again, can be a string, number)
+tennis_players.append("Switzerland")
+print(tennis_players)
+# Create a tuple with 4 items (can be strings, numbers, both)
+hockey_players = ("Erik Karlsson", "Defenseman", 31, "San Jose Sharks")
+print(hockey_players)
